3678f4a8e4f8977df1ae29eeb82b55b1f3f94099
Taha <tahabouataoui5@gmail.com>
2024-03-10 14:58:43 +0000

    added java12

--- src/main/java/com/example/devoir_jsf/util/DbUtil.java
+++ src/main/java/com/example/devoir_jsf/util/DbUtil.java
@@ -1,14 +1,16 @@
 package com.example.devoir_jsf.util;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtil {
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/employee?serverTimezone=UTC";
-    private static final String USER = "taha";
-    private static final String PASS = "password";
+//    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+//    private static final String DB_URL = "jdbc:mysql://localhost:3306/employee?serverTimezone=UTC";
+//    private static final String USER = "taha";
+//    private static final String PASS = "password";
 
     private static Connection connection = null;
 
@@ -16,16 +18,22 @@ public class DbUtil {
     }
 
     public static Connection getConnection() {
-       try{
-           Connection connection ;
-                        Class.forName(JDBC_DRIVER);
-                        connection = DriverManager.getConnection(DB_URL, USER, PASS);
-                        return connection;
-                    } catch (ClassNotFoundException| SQLException e) {
-                        throw new RuntimeException("Error connecting to the database", e);
-                    }
+        if (connection != null) {
+            return connection;
+        }
+        try {
+            String databaseUrl = System.getenv("DATABASE_URL");
+            URI dbUri = new URI(databaseUrl);
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
 
+            return DriverManager.getConnection(dbUrl, username, password);
+        } catch (URISyntaxException | SQLException e) {
+            throw new RuntimeException("Error connecting to the database", e);
+        }
     }
+
     // You can also add a closeConnection method if you want to close the connection
     public static void closeConnection() {
         if (connection != null) {
