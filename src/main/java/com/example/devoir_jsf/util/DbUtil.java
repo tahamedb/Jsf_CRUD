993bfeafbb725a1d22001a3eff2b7dd0575ba870
Taha <tahabouataoui5@gmail.com>
2024-03-10 17:20:25 +0000

    Prepare for deployment to Heroku v8

--- src/main/java/com/example/devoir_jsf/util/DbUtil.java
+++ src/main/java/com/example/devoir_jsf/util/DbUtil.java
@@ -1,46 +1,39 @@
 package com.example.devoir_jsf.util;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbUtil {
-//    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-//    private static final String DB_URL = "jdbc:mysql://localhost:3306/employee?serverTimezone=UTC";
-//    private static final String USER = "taha";
-//    private static final String PASS = "password";
 
     private static Connection connection = null;
-
-    public DbUtil() {
-    }
 
     public static Connection getConnection() {
         if (connection != null) {
             return connection;
-        }
-        try {
-            String databaseUrl = System.getenv("DATABASE_URL");
-            if (databaseUrl == null) {
-                throw new SQLException("DATABASE_URL not set in the environment variables");
+        } else {
+            try {
+                // PostgreSQL JDBC driver class
+                Class.forName("org.postgresql.Driver");
+
+                // Connection details
+                String host = "cc3engiv0mo271.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
+                String database = "d31fehukfeqjil";
+                String user = "ubi35vedhn7665";
+                String port = "5432";
+                String password = "pacaa553e6aea08be29de407ab72061b8a8a2e2504cc7c0a8bcea18ff8e2f4896";
+                String dbUrl = "jdbc:postgresql://" + host + ':' + port + "/" + database;
+
+                // Establishing the connection
+                connection = DriverManager.getConnection(dbUrl, user, password);
+
+            } catch (ClassNotFoundException | SQLException e) {
+                throw new RuntimeException("Error connecting to the database", e);
             }
-            URI dbUri = new URI(databaseUrl);
-
-            String username = dbUri.getUserInfo().split(":")[0];
-            String password = dbUri.getUserInfo().split(":")[1];
-            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' +
-                    dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
-
-            connection = DriverManager.getConnection(dbUrl, username, password);
             return connection;
-        } catch (URISyntaxException | SQLException e) {
-            throw new RuntimeException("Error connecting to the database", e);
         }
     }
 
-    // You can also add a closeConnection method if you want to close the connection
     public static void closeConnection() {
         if (connection != null) {
             try {
@@ -48,8 +41,8 @@ public class DbUtil {
                 connection = null;
             } catch (SQLException e) {
                 // Handle closing error
+                e.printStackTrace();
             }
         }
     }
 }
-
